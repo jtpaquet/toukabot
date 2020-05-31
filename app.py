89be213dbfe9412d59b529d6e66d5ba516ce2e64
Jérémy Talbot-Pâquet <jtpaquet@gmail.com>
2020-05-31 14:03:36 -0400

    created utils.py

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ import sys
 import os
 import json
 from Credentials import *
+from pymessenger import Bot
+
 
 app = Flask(__name__)
+
+bot = Bot(PAGE_ACCESS_TOKEN)
 
 
 @app.route('/', methods=['GET'])
@@ -22,6 +26,34 @@ def handle_verification():
 def receive_message():
     data = request.get_json()
     log(data)
+
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
+
+                # IDs
+                sender_id = messaging_event['sender']['id']
+                recipient_id = messaging_event['recipient']['id']
+
+                if messaging_event.get('message'):
+                    # Extracting text message
+                    if 'text' in messaging_event['message']:
+                        messaging_text = messaging_event['message']['text']
+                    else:
+                        messaging_text = 'no text'
+
+                    response = None
+
+                    entity, value = wit_response(messaging_text)
+                    if entity == 'newstype':
+                        response = "Ok, I will send you the {} news".format(str(value))
+                    elif entity == 'location':
+                        response = "Ok, so you live in {0}. Here are top headlines from {0}".format(str(value))
+
+                    if response == None:
+                        response = "I have no idea what you are saying!"
+                        
+                    bot.send_text_message(sender_id, response)
     return "ok", 200
 
 
